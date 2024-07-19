2a6c5bacff2f0079e7306a672ca2b5149f69e042
golivesid <155578467+golivesid@users.noreply.github.com>
2024-07-19 22:36:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const snapsave = require('./snapsave-downloader');
 const port = 3000;
 
-app.get('/tera', (req, res) => {
+app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
 
